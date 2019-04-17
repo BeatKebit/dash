2aa564352982feb548f0e04b4a27e802afb7e800
Carlos Iván Ibarra Pacheco <bytepacheco@gmail.com>
2019-04-17 14:28:52 -0500

    Private Pac re design
    moved from Overview now works

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -97,6 +97,7 @@ private:
     QMenuBar *appMenuBar;
     QAction *overviewAction;
     QAction *historyAction;
+    QAction *privateAction;
     QAction *masternodeAction;
     QAction *quitAction;
     QAction *sendCoinsAction;
@@ -234,6 +235,8 @@ private Q_SLOTS:
     void gotoOverviewPage();
     /** Switch to history (transactions) page */
     void gotoHistoryPage();
+    /** Switch to private (section) page */
+    void gotoPrivatePage();
     /** Switch to masternode page */
     void gotoMasternodePage();
     /** Switch to receive coins page */
