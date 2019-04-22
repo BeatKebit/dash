010bb5f27d084774e98ad74fccca39740e2775a4
Kevin <kevin_defimex@hotmail.com>
2019-04-22 11:49:04 -0500

    Update the PAC value on the views
    Only Send Page working, on receive and overview pages we need to add the UI containers to print the value.

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -197,6 +197,9 @@ Q_SIGNALS:
     /** Restart handling */
     void requestedRestart(QStringList args);
 
+    /** Refresh PAC value to the walletframe */
+    void transmit_to_walletframe();
+
 public Q_SLOTS:
     /** Set number of connections shown in the UI */
     void setNumConnections(int count);
@@ -258,8 +261,7 @@ private Q_SLOTS:
     void gotoVerifyMessageTab(QString addr = "");
 
     /** API PAC_USD request */
-    //Rubik
-    //void managerCurrencyFinished(QNetworkReply *replyC);
+    void managerCurrencyFinished(QNetworkReply *replyC);
     /** API News request */
     void managerNewsFinished(QNetworkReply *replyN);
     /** Copy the text of the news */
